d900d997d96ad65ff5289e8d3083e9fd129c258d
Karla <klopezm161@ulacit.ed.cr>
2020-07-12 21:29:26 -0600

    Login listo, bienvenida lista con roles, producto listo con roles

---
+++ DiseñoWeb1Intento1Intranet/DiseñoWeb1Intento1Intranet/JS/Bienvenida.js
@@ -0,0 +1,74 @@
+﻿window.onload = function () {
+    console.log("Entró al onload");
+    var usuario = localStorage['user'];  
+    console.log("dentro de load de bienvinido");
+    console.log(usuario);
+    const URLGet = "https://localhost:44308/api/UsuarioRol/" + usuario;
+    list(URLGet).catch((e) => console.error(e));
+};
+let ArrRoles = [];
+
+async function list(Get = "") {
+    try {
+        const objetoRecibido = await request(Get);
+        const objetoList = await JSON.parse(objetoRecibido);
+        generarNav(objetoList);
+
+    } catch (err) {
+        console.log('Error: ' + err);
+    }
+}
+
+function request(url) {
+    return new Promise(function (resolve, reject) {
+        const xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function (e) {
+            if (xhr.readyState === 4) {
+                if (xhr.status === 200) {
+                    resolve(xhr.response);
+                } else {
+                    reject(xhr.status);
+                }
+            }
+        };
+        xhr.ontimeout = function () {
+            reject("timeout");
+        };
+        xhr.open("get", url, true);
+        xhr.send();
+    });
+}
+
+function generarNav(datos) {
+    var cont = 0;//se utiliza para obtener indice por editar
+    for (let valor of JSON.parse(datos)) {
+        if (valor.codigo_rol == 1) {
+            document.getElementById('navegacion_seguridad').style.display = "block";
+            document.getElementById('navegacion_administracion').style.display = "block";
+            document.getElementById('navegacion_consulta').style.display = "block";
+        }
+        if (valor.codigo_rol == 2) {
+            document.getElementById('navegacion_seguridad').style.display = "block";
+        }
+        if (valor.codigo_rol == 3) {
+            document.getElementById('navegacion_administracion').style.display = "block";
+        }
+        if (valor.codigo_rol == 4) {
+            document.getElementById('navegacion_consulta').style.display = "block";
+        }
+
+        ArrRoles.push(valor);
+    }
+    localStorage.setItem('rolesUsuario', JSON.stringify(ArrRoles));
+
+    console.log("Imprimiendo el array");
+    console.log(ArrRoles)
+    console.log("Imprimiendo índice de array");
+    console.log(ArrRoles[0])
+}
+
+
+
+
+
+//window.location.href = '/Administracion/EdicionNuevoConsecutivo ';
---
+++ DiseñoWeb1Intento1Intranet/DiseñoWeb1Intento1Intranet/JS/Seguridad/RolPermiso.js
@@ -0,0 +1,39 @@
+﻿var id = localStorage['user'];
+users = JSON.parse(users);
+
+var rolesAcceso = ['Administrador', 'Seguridad'];//Cambiar aquí los roles permitidos
+
+var rolesUsuario = [];//Roles del usuario activo
+
+var obtenerRoles = () => {
+    fetch('https://localhost:44308/api/UsuariRol/' + id)
+        .then(res => res.json())
+        .then(data => {
+            const json_data = JSON.parse(data);
+            rolesUsuario = json_data;
+            validar();
+        })
+        .catch(err => console.log('error', err))
+}
+
+var permiso;
+var validar = () => {
+    permiso = false;
+    for (i = 0; i < rolesUsuario.length; i++) {
+        for (j = 0; j < rolesAcceso.length; j++) {
+            if (rolesUsuario[i].descripcion == rolesAcceso[j]) {//Si tiene permiso
+                permiso = true;
+            }
+        }
+    }
+
+    if (permiso) {
+    } else {
+        alert('Alerta!! no estas autorizado para acceder a esta página');
+        $(document).ready(function () {
+            window.setTimeout(function () {
+                location.href = "index.html";
+            }, 3000);
+        });
+    }
+}
